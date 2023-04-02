32e3504c106c2b33b62be248e41ed44185ada649
ColeStolt <stoltcol2000@gmail.com>
2023-04-02 14:55:37 -0500

    Added basic client entry
    Added some basic client entry stuff. Still need to work on parsing the
    data a bit so weird numbers don't happen by accident.

--- Swim_Scheduler/src/code/controllers/ClientEntryController.java
+++ Swim_Scheduler/src/code/controllers/ClientEntryController.java
@@ -1,17 +1,24 @@
 package code.controllers;
 
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 
 import code.customUI.MaskedTextField;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
+import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 
 /**
@@ -30,23 +37,37 @@ public class ClientEntryController {
 	@FXML private Button cancelButton;
 	@FXML private Button saveButton;
 	@FXML private VBox clientInfoEntriesVBox;
+	@FXML private VBox clientLessonInfoEntryVBox;
 	
 	// Fields
 	private TextField nameField;
 	private MaskedTextField phoneNumberField;
 	private TextField addressField;
 	private MaskedTextField kidsField;
-	private ChoiceBox instructorField;
+	private ChoiceBox<String> instructorChoice;
+	private MaskedTextField numberOfLessonsField;
+	private MaskedTextField amountPerLessonField;
+	private RadioButton paidInFullRadio;
 	
+	// Panes
+	HBox radioAndLabel;
+	BorderPane titlePane;
 	
 	// Labels
 	private Label nameLabel;
 	private Label phoneNumberLabel;
 	private Label addressLabel;
 	private Label kidsLabel;
+	private Label instructorLabel;
+	private Label numberOfLessonsLabel;
+	private Label amountPerLessonLabel;
+	private Label paidInFullLabel;
+	private Label clientInfoTitle;
+	private Label lessonInfoTitle;
 	
 	public void initialize() {
 		instructorInfoSetup();
+		lessonInfoSetup();
 	}
 	
 	public void cancelClientAdd() {
@@ -62,7 +83,18 @@ public class ClientEntryController {
 	public void saveClient() {
 		
 		try {
-			BufferedWriter writeData = new BufferedWriter(new FileWriter("src\\data\\client.data"));
+			BufferedWriter writeData = new BufferedWriter(new FileWriter("src\\data\\client.data", true));
+			writeData.write(nameField.getText() + "`"
+					+ phoneNumberField.getText() + "`"
+					+ addressField.getText() + "`"
+					+ parseData(kidsField.getText()) + "`"
+					+ instructorChoice.getValue() + "`"
+					+ parseData(numberOfLessonsField.getText()) + "`"
+					+ parseData(amountPerLessonField.getText()) + "`"
+					+ paidInFullRadio.isSelected() + "\n");
+			writeData.close();
+			mainScenePane.setCenter(FXMLLoader.load(getClass().getResource("/resources/scenes/ClientsScene.fxml")));
+			mainScenePane.setBottom(null);
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -70,8 +102,33 @@ public class ClientEntryController {
 		
 	}
 	
-	public void populateChoiceBox() {
+	public String parseData(String data) {
 		
+		data = data.replace("-", "0");
+		data = data.replace("$", "");
+		return data;
+	}
+	
+	public void populateChoiceBox(ChoiceBox<String> choiceBox) {
+		try {
+				BufferedReader dataFile = new BufferedReader(new FileReader("src\\data\\instructorNames.data"));
+				String dataLine;
+				while((dataLine = dataFile.readLine()) != null) {
+					choiceBox.getItems().add(dataLine);
+			}
+			
+			dataFile.close();
+			
+		} catch (FileNotFoundException e) {
+			// Show error to user if file is not located
+			Alert sceneAlert = new Alert(AlertType.ERROR);
+			sceneAlert.setContentText("Could not load the \"instructorNames.data\" file.\nContact the developer about this issue.");
+			sceneAlert.show();
+		} catch (IOException e) {
+			Alert sceneAlert = new Alert(AlertType.ERROR);
+			sceneAlert.setContentText("Could not parse data file in \"instructorNames \".\nContact the developer about this issue.");
+			sceneAlert.show();
+		}
 	}
 	
 	public void instructorInfoSetup() {
@@ -80,28 +137,38 @@ public class ClientEntryController {
 		nameField = new TextField();
 		phoneNumberField = new MaskedTextField("(###) ###-####");
 		addressField = new TextField();
-		kidsField = new MaskedTextField("###");
+		kidsField = new MaskedTextField("#", '-');
+		
+		// Declare panes
+		titlePane = new BorderPane();
 		
 		// Declare Labels
 		nameLabel = new Label("Client Name:");
 		phoneNumberLabel = new Label("Phone Number:");
 		addressLabel = new Label("Address Of Lessons:");
 		kidsLabel = new Label("Number Of Kids:");
+		clientInfoTitle = new Label("Client Info");
 		
 		// Label Settings
 		phoneNumberLabel.setId("labels");
 		nameLabel.setId("labels");
 		addressLabel.setId("labels");
 		kidsLabel.setId("labels");
+		clientInfoTitle.setId("clientInfoTitle");
 		
 		// Field Settings
 		nameField.setId("fields");
 		phoneNumberField.setId("phoneNumberField");
-		phoneNumberField.setPlaceholder('_');
 		addressField.setId("fields");
 		kidsField.setId("fields");
-		kidsField.setPlaceholder('\0');
 		
+		// Pane settings
+		titlePane.setId("titlePane");
+		
+		// Add to panes
+		titlePane.setCenter(clientInfoTitle);
+		
+		clientInfoEntriesVBox.getChildren().add(titlePane);
 		clientInfoEntriesVBox.getChildren().add(nameLabel);
 		clientInfoEntriesVBox.getChildren().add(nameField);
 		clientInfoEntriesVBox.getChildren().add(phoneNumberLabel);
@@ -113,7 +180,60 @@ public class ClientEntryController {
 	}
 	
 	public void lessonInfoSetup() {
+		// Declare Fields
+		instructorChoice = new ChoiceBox<String>();
+		numberOfLessonsField = new MaskedTextField("##", '-');
+		amountPerLessonField = new MaskedTextField("$###.00", '-');
+		paidInFullRadio = new RadioButton();
+	
 		
+		// Declare Labels
+		instructorLabel = new Label("Instructor:");
+		numberOfLessonsLabel = new Label("Number Of Lessons (Per Kid):");
+		amountPerLessonLabel = new Label("Amount Per Lesson: ");
+		paidInFullLabel = new Label("Paid In Full:");
+		lessonInfoTitle = new Label("Lesson Info");
+		
+		// Label Settings
+		instructorLabel.setId("labels");
+		numberOfLessonsLabel.setId("labels");
+		amountPerLessonLabel.setId("labels");
+		paidInFullLabel.setId("labels");
+		lessonInfoTitle.setId("lessonInfoTitle");
+		
+		// Choicebox settings
+		instructorChoice.setId("choiceBox");
+		instructorChoice.setValue("Niko");
+		populateChoiceBox(instructorChoice);
+		
+		// Field settings
+		numberOfLessonsField.setId("fields");
+		amountPerLessonField.setId("fields");
+		
+		// Radio settings
+		paidInFullRadio.setId("radioButton");
+		
+		// Declare Panes
+		radioAndLabel = new HBox();
+		titlePane = new BorderPane();
+		
+		// Pane settings
+		titlePane.setId("titlePane");
+		
+		// Add to panes
+		radioAndLabel.getChildren().add(paidInFullLabel);
+		radioAndLabel.getChildren().add(paidInFullRadio);
+		titlePane.setCenter(lessonInfoTitle);
+		
+		// Add to vbox
+		clientLessonInfoEntryVBox.getChildren().add(titlePane);
+		clientLessonInfoEntryVBox.getChildren().add(instructorLabel);
+		clientLessonInfoEntryVBox.getChildren().add(instructorChoice);
+		clientLessonInfoEntryVBox.getChildren().add(numberOfLessonsLabel);
+		clientLessonInfoEntryVBox.getChildren().add(numberOfLessonsField);
+		clientLessonInfoEntryVBox.getChildren().add(amountPerLessonLabel);
+		clientLessonInfoEntryVBox.getChildren().add(amountPerLessonField);
+		clientLessonInfoEntryVBox.getChildren().add(radioAndLabel);
 	}
 	
 }
