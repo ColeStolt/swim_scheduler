7bfcc07288b72d282f50fc28fd8204298a862169
ColeStolt <stoltcol2000@gmail.com>
2023-04-01 20:23:53 -0500

    Implemented a simple database that reads from a file. Added a search bar to the client list. Made a small tweak to the client card style.

--- Swim_Scheduler/src/code/controllers/ClientSceneController.java
+++ Swim_Scheduler/src/code/controllers/ClientSceneController.java
@@ -1,10 +1,11 @@
 package code.controllers;
 
-import code.dataObjects.Client;
+import code.datapersistance_dao.ClientDataDB;
 import code.services.ClientCard;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.VBox;
 
@@ -13,11 +14,37 @@ public class ClientSceneController {
 	@FXML Button addClientButton;
 	@FXML VBox scrollPaneVBox;
 	@FXML BorderPane mainClientScenePane;
+	@FXML TextField clientSearchBar;
+	
+	private ClientDataDB clientData;
+	
+	public void initialize() {
+		clientData = new ClientDataDB("src\\data\\client.data");
+		
+		// init settings 
+		scrollPaneVBox.setFillWidth(true);
+		
+		// Iterate through list and add client card to screen
+		for(int i = 0; i < clientData.getClientDB().size(); i++) {
+			scrollPaneVBox.getChildren().add(new ClientCard(clientData.getClientDB().get(i)));
+		}
+
+	}
 	
 	public void addButt(ActionEvent e) {
 		
-		scrollPaneVBox.setFillWidth(true);
-		scrollPaneVBox.getChildren().add(new ClientCard(new Client("Taco Bell", "2345 Bean Burrito dr.", "123-456-7890", (short)5, "Yung G", (short)12, 125, false)));
 	}
-	// Comment
+	
+	// Update the client list as user type (VERY NEAT)
+	// Remove all clients when key typed
+	// Search list for client names that contain the text
+	// Fill list with appropriate client cards
+	public void updateClientList() {
+		scrollPaneVBox.getChildren().removeAll(scrollPaneVBox.getChildren());
+		for(int i = 0; i < clientData.getClientDB().size(); i++) {
+			if(clientData.getClientDB().get(i).getClientName().toLowerCase().contains(clientSearchBar.getText().toLowerCase())) {
+				scrollPaneVBox.getChildren().add(new ClientCard(clientData.getClientDB().get(i)));
+			}
+		}
+	}
 }
--- Swim_Scheduler/src/code/datapersistance_dao/ClientDataDB.java
+++ Swim_Scheduler/src/code/datapersistance_dao/ClientDataDB.java
@@ -1,24 +1,69 @@
 package code.datapersistance_dao;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import code.dataObjects.Client;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+
 /**
  * 
  * @author Cole Stoltz
- * This will be a singleton class containing the data of clients.
- * Data will be loaded from a file into a hash table. Data will
- * also be inserted into the hash table. This singleton will have
- * read write authorities for client .data files.
+ * This will act as a database. Since this will be a simple database with less <1000
+ * data will have to be reloaded each instance of its creation. When a controller
+ * uses this class it will create an instance I.E. a new database. It takes data
+ * from the .data files fills this database, changes what it needs to change and
+ * then restores that data for later manipulation.
  *
  */
 
 public class ClientDataDB {
-
 	
+	// Table
+	private ArrayList<Client> ClientData = new ArrayList<Client>();
 	
 	// Constructor
-	private ClientDataDB() {
+	public ClientDataDB(String filePath) {
+		readAndFill(filePath);
+	}
+	
+	// Read from file and fill list table
+	public void readAndFill(String filePath) {
+		try {
+			BufferedReader dataFile = new BufferedReader(new FileReader(filePath));
+			String dataLine;
+			String tokens[];
+			while((dataLine = dataFile.readLine()) != null) {
+				tokens = dataLine.split("`"); // Chose a more obscure delimiter to avoid issues
+				Client tempClient = new Client(tokens[0], tokens[1], tokens[2], Short.parseShort(tokens[3]), tokens[4], Short.parseShort(tokens[5]), Float.parseFloat(tokens[6]), Boolean.parseBoolean(tokens[7]));
+				ClientData.add(tempClient);
+			}
+			
+			dataFile.close();
+			
+		} catch (FileNotFoundException e) {
+			// Show error to user if file is not located
+			Alert sceneAlert = new Alert(AlertType.ERROR);
+			sceneAlert.setContentText("Could not load the \"client.data\" file.\nContact the developer about this issue.");
+			sceneAlert.show();
+		} catch (IOException e) {
+			Alert sceneAlert = new Alert(AlertType.ERROR);
+			sceneAlert.setContentText("Could not parse data file in \"client DB\".\nContact the developer about this issue.");
+			sceneAlert.show();
+		}
+	}
+	
+
+	// Store (save) data to file
+	public void saveData() {
 		
 	}
-	// Read and fill hash table
-	// Add to table and store
 	
+	// Setters / Getters
+	public ArrayList<Client> getClientDB(){
+		return ClientData;
+	}
 }
