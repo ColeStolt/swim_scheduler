651fec6126f14ff4ffba9f27434e6342064b165e
ColeStolt <stoltcol2000@gmail.com>
2023-04-06 09:07:07 -0500

    Testing serialization.

--- Swim_Scheduler/src/code/datapersistance_dao/ClientDataDB.java
+++ Swim_Scheduler/src/code/datapersistance_dao/ClientDataDB.java
@@ -1,69 +1,92 @@
 package code.datapersistance_dao;
 
-import java.io.BufferedReader;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.io.FileReader;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.util.ArrayList;
+
 import code.dataObjects.Client;
-import javafx.scene.control.Alert;
-import javafx.scene.control.Alert.AlertType;
 
 /**
  * 
  * @author Cole Stoltz
- * This will act as a database. Since this will be a simple database with less <1000
- * data will have to be reloaded each instance of its creation. When a controller
- * uses this class it will create an instance I.E. a new database. It takes data
- * from the .data files fills this database, changes what it needs to change and
- * then restores that data for later manipulation.
+ * This will act as a database. It is a singleton that multiple classes
+ * can access. There will only be one instance to access and data will
+ * be serialized vs. storing individually. Serializing data just turns
+ * the object into a byte stream.
  *
  */
 
-public class ClientDataDB {
+public class ClientDataDB implements java.io.Serializable {
 	
-	// Table
-	private ArrayList<Client> ClientData = new ArrayList<Client>();
+
+	private static final long serialVersionUID = -5168600904513136308L;
+
+	private static final ClientDataDB instance = new ClientDataDB();
+	
+	private ArrayList<Client> clientData = new ArrayList<Client>();
+	
+	private String filePath = "src\\data\\client.data";
 	
 	// Constructor
-	public ClientDataDB(String filePath) {
-		readAndFill(filePath);
+	private ClientDataDB() {
+		readAndFill();
 	}
 	
 	// Read from file and fill list table
-	public void readAndFill(String filePath) {
+	@SuppressWarnings("unchecked")
+	// Do not need to check if it is an arraylist object
+	// This will be the only object stored in the client.data
+	// file.
+	public void readAndFill() {
 		try {
-			BufferedReader dataFile = new BufferedReader(new FileReader(filePath));
-			String dataLine;
-			String tokens[];
-			while((dataLine = dataFile.readLine()) != null) {
-				tokens = dataLine.split("`"); // Chose a more obscure delimiter to avoid issues
-				Client tempClient = new Client(tokens[0], tokens[1], tokens[2], Short.parseShort(tokens[3]), tokens[4], Short.parseShort(tokens[5]), Float.parseFloat(tokens[6]), Boolean.parseBoolean(tokens[7]));
-				ClientData.add(tempClient);
-			}
+			FileInputStream dataFile = new FileInputStream(filePath);
+			ObjectInputStream arrayObject = new ObjectInputStream(dataFile);
+			
+			clientData = ((ArrayList<Client>)arrayObject.readObject());
 			
 			dataFile.close();
+			arrayObject.close();
 			
 		} catch (FileNotFoundException e) {
-			// Show error to user if file is not located
-			Alert sceneAlert = new Alert(AlertType.ERROR);
-			sceneAlert.setContentText("Could not load the \"client.data\" file.\nContact the developer about this issue.");
-			sceneAlert.show();
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		} catch (IOException e) {
-			Alert sceneAlert = new Alert(AlertType.ERROR);
-			sceneAlert.setContentText("Could not parse data file in \"client DB\".\nContact the developer about this issue.");
-			sceneAlert.show();
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
 	}
 	
 
 	// Store (save) data to file
+	@SuppressWarnings("unchecked")
 	public void saveData() {
-		
+		try {
+			FileOutputStream dataFile = new FileOutputStream(filePath);
+			ObjectOutputStream arrayObjectOut = new ObjectOutputStream(dataFile);
+			
+			arrayObjectOut.writeObject(clientData);
+			
+			dataFile.close();
+			arrayObjectOut.close();
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
 	}
 	
 	// Setters / Getters
 	public ArrayList<Client> getClientDB(){
-		return ClientData;
+		return clientData;
 	}
 }
