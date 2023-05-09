e026cd8863adfe927c1ced2a24d68ed5c1c2db43
ColeStolt <stoltcol2000@gmail.com>
2023-05-08 21:05:35 -0500

    Minor adjustments to scheduling.

--- Swim_Scheduler/src/code/controllers/ScheduleController.java
+++ Swim_Scheduler/src/code/controllers/ScheduleController.java
@@ -1,13 +1,20 @@
 package code.controllers;
 
+import java.io.IOException;
+import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.Locale;
 
 import code.customUI.MaskedTextField;
+import code.datapersistance_dao.ClientCardReferenceSingleton;
+import code.datapersistance_dao.ClientDataDB;
+import code.datapersistance_dao.MainScreenSingleton;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.DatePicker;
+import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
 
 public class ScheduleController {
@@ -23,13 +30,21 @@ public class ScheduleController {
 	@FXML BorderPane timeBorderpane;
 	@FXML DatePicker startDatePicker;
 	@FXML ComboBox<String> morningNoonComboBox;
+	@FXML Label clientName;
 	
 	MaskedTextField timeField;
+	
+	// Singletons
+	private ClientDataDB clientDB = ClientDataDB.getInstance();
+	private MainScreenSingleton mainScreen = MainScreenSingleton.getInstance();
+	private ClientCardReferenceSingleton clientTempReference = ClientCardReferenceSingleton.getInstance();
 	
 	public void initialize() {
 		
 		timeOfLessonsSetup();
 		comboBoxSetup();
+		clientName.setText(clientTempReference.getClient().getClientName());
+		startDatePicker.setValue(LocalDate.now());
 	}
 	
 	void timeOfLessonsSetup() {
@@ -51,6 +66,17 @@ public class ScheduleController {
 		morningNoonComboBox.getItems().add("AM");
 		morningNoonComboBox.getItems().add("PM");
 		morningNoonComboBox.setValue("AM");
+	}
+	
+	public void cancelSchedule() {
+		try {
+			clientTempReference.setClientReference(null);
+			mainScreen.getPane().setCenter(FXMLLoader.load(getClass().getResource("/resources/scenes/ScheduledAndUnscheduledScene.fxml")));
+			//mainScenePane.setBottom(null);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 	
 }
